b289ce4f50c0e2fa3e6790d1cfd23b202adb8e3b
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2022-05-02 18:19:29 -0500

    started, running
    most barebones implementation possible, but it works.

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mint(mintAmount)
+      .MintStrummer()
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
